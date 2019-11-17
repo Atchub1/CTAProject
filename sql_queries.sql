9daf5c3ddf88bb08533d57e23fb7d5fa9f8d8761
Atchub1 <amytcleveland@gmail.com>
2019-11-16 18:23:00 -0600

    update

--- sql_queries.sql
+++ sql_queries.sql
@@ -3,6 +3,8 @@ ALTER TABLE weekday_data ADD PRIMARY KEY ("Station_ID");
 ALTER TABLE saturday_data ADD PRIMARY KEY ("Station_ID");
 ALTER TABLE sunday_holiday_data ADD PRIMARY KEY ("Station_ID");
 ALTER TABLE ten_year_ridership ADD PRIMARY KEY ("Station_ID");
+ALTER TABLE twitter_sentiment ALTER COLUMN "Date" TYPE date;
+ALTER TABLE twitter_sentiment ADD PRIMARY KEY ("Tweet_ID");
 
 SELECT * FROM total_ridership;
 
